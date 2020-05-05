879b86d04793dbd3c06b7a122ec0e129445dd9bb
StrongFlaky <strongli2014@hotmail.com>
2020-05-05 21:47:20 +0000

    done

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -5,12 +5,21 @@
  * Implement insert() correctly
  */
 void BloomFilter::insert(const string & s) {
-    /* YOUR CODE HERE */
+    for(int i = 0; i < K; i++){
+        int index = hash_functions[i](s)%M;
+        bits[index] = true;
+    }
 }
 
 /**
  * Implement find() correctly
  */
 bool BloomFilter::find(const string & s) {
-    /* YOUR CODE HERE */
+    for(int i = 0; i < K; i++){
+        int index = hash_functions[i](s)%M;
+        if (bits[index] == false){
+            return false; 
+        }
+    }
+    return true;
 }
--- CountMinSketch.cpp
+++ CountMinSketch.cpp
@@ -6,12 +6,23 @@
  * Implement increment() correctly
  */
 void CountMinSketch::increment(const string & s) {
-    /* YOUR CODE HERE */
+    for(int i = 0; i < K; i++){
+        int index = hash_functions[i](s)%M;
+        count[i][index] += 1;
+    }
 }
 
 /**
  * Implement find() correctly
  */
 unsigned int CountMinSketch::find(const string & s) {
-    /* YOUR CODE HERE */
+    int check = std::numeric_limits<int>::max();
+    for(int i = 0; i < K; i++){
+        int index = hash_functions[i](s)%M;
+        int current = count[i][index];
+        if (current < check){
+            check = current;
+        }
+    }
+    return check; 
 }
